2965de7fe2b733590260ea581d193069f95d99e7
darkLord <ismoiljoraxonov1@gmail.com>
2026-04-27 14:32:54 +0900

    ismoil added tom and it is white

--- src/main/java/com/example/childsecurity/model/Cat.java
+++ src/main/java/com/example/childsecurity/model/Cat.java
@@ -2,7 +2,15 @@ package com.example.childsecurity.model;
 
 public class Cat {
 
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
     public static void main(String[] args) {
 
+        Cat cat = new Cat("Tom");
+        System.out.println(cat.name+" Tom is a cat . It is white and has a tail");
     }
 }
