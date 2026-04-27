c03689c497162fe9eb1cb51170559febbe2de78a
darkLord <ismoiljoraxonov1@gmail.com>
2026-04-27 13:40:36 +0900

    update ismoil

--- src/main/java/com/example/childsecurity/model/Cat.java
+++ src/main/java/com/example/childsecurity/model/Cat.java
@@ -3,10 +3,6 @@ package com.example.childsecurity.model;
 public class Cat {
 
     public static void main(String[] args) {
-        System.out.println("Cat");
-
-
-        System.out.println("Ismoil qoshdi");
 
     }
 }
