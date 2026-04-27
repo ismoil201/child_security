11a26af13b8d983adefaa933c0eddbc4887f9d24
darkLord <ismoiljoraxonov1@gmail.com>
2026-04-27 14:30:25 +0900

    Charos added Rapunsel is black

--- src/main/java/com/example/childsecurity/model/Cat.java
+++ src/main/java/com/example/childsecurity/model/Cat.java
@@ -2,11 +2,23 @@ package com.example.childsecurity.model;
 
 public class Cat {
 
+     String name;
+    Cat() {
+        System.out.println("Cat is black");
+    }
     public static void main(String[] args) {
         System.out.println("Cat");
 
 
         System.out.println("Ismoil qoshdi");
+        Cat cat1 = new Cat();
+        cat1.name = "Rapunsel";
 
     }
+
+
+
+    Cat cat2 = new Cat();
+
+
 }
